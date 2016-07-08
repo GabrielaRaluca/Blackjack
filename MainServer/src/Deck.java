805efd394d0bbed9050a514f7fb4fb40992c7a6f
babadela3 <teosposib@yahoo.com>
2016-07-09 00:52:11 +0300

    Initial commit

---
+++ MainServer/src/Deck.java
@@ -0,0 +1,6 @@
+/**
+ * Created by teo on 08.07.2016.
+ */
+public class Deck {
+    public void shuffle(){}
+}
---
+++ MainServer/src/Player.java
@@ -0,0 +1,7 @@
+/**
+ * Created by teo on 08.07.2016.
+ */
+public class Player {
+   public synchronized void getcard(Deck object){}
+}
+
